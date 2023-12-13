0816be0c11bbf1a15c3fb6d65e16012ddaf47ddc
Muhammad Rifqi Fauzan <135447075+murfa414@users.noreply.github.com>
2023-12-13 16:19:59 +0700

    Add files via upload

---
+++ rumuspersegipanjang.py
@@ -0,0 +1,11 @@
+print("LUAS PERSEGI PANJANG")
+panjang = int(input("Berapa panjangnya? "))
+lebar = int(input("Berapa lebarnya? "))
+luas = panjang*lebar
+print("Luas persegi panjang adalah:",luas)
+
+print("\nKELILING PERSEGI PANJANG")
+anjang = int(input("Berapa panjangnya? "))
+lebar = int(input("Berapa lebarnya? "))
+keliling = 2*(panjang+lebar)
+print("Keliling persegi panjang adalah:",keliling)
